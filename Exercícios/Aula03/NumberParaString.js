e6450e41296034041e34026c94d9a9c6238b6e48
LeoSouzaSenac <152904287+LeoSouzaSenac@users.noreply.github.com>
2024-03-14 19:40:48 -0300

    Add files via upload

---
+++ Exercícios/Aula03/NumberParaString.js
@@ -0,0 +1,5 @@
+let numero = 10
+
+numeroEmString = numero.toString()
+
+console.log(typeof numeroEmString)
---
+++ Exercícios/Aula03/StringParaNumber.js
@@ -0,0 +1,8 @@
+let numeroString = "10"
+let numeroString2 = "20"
+
+let numero = Number(numeroString)
+
+console.log(typeof numero)
+
+
