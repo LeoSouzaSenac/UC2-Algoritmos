53373a9cd32b23aa59e35f4975da9d4fd6c7890e
LeoSouzaSenac <152904287+LeoSouzaSenac@users.noreply.github.com>
2024-03-14 21:18:43 -0300

    Add files via upload

---
+++ Exercícios/Aula05/comparadores.js
@@ -0,0 +1,57 @@
+const variavel1 = 12
+const variavel2 = 35
+
+//Igual: === 
+//verifica se dois valores são iguais
+console.log(variavel1 === (2*25)) // true
+console.log(variavel1 === 50) //true
+console.log(variavel1 === variavel2) //false
+
+//Diferente: !==
+//verifica se dois valores são diferentes
+console.log(variavel1 !== variavel2)//true
+
+
+//Maior >
+//Verifica se o valor da esquerda é maior que o valor da direita
+console.log(variavel1 > variavel2) //true
+console.log(variavel2 > variavel1) //false
+
+//Maior ou igual:   >=
+//Verifica se o valor da esquerda é maior OU é igual ao valor da direita
+console.log(variavel1 >= variavel2) //true
+console.log(50 >= 35) //true
+console.log(50 >= 50) //true
+console.log(35 >= 50) //false
+
+//Menor <
+//verifica se o valor da esquerda é menor que o da direita
+console.log(variavel1 < variavel2) //false
+console.log(23 < 90) //true
+
+//Menor ou igual <=
+//verifica se o valor da esquerda é menor OU igual ao da direita
+console.log(variavel1 <= variavel2) //false
+console.log(variavel1 <= 50) //true
+
+
+/*
+
+
+
+Crie duas variáveis que guardem dois números. Imprima na tela as seguintes mensagens:
+
+O primeiro número é igual ao segundo? True/False
+O primeiro número é diferente do segundo? True/False
+O primeiro número é maior que o segundo? True/False
+O primeiro número é menor que o segundo? True/False
+
+
+
+
+*/
+
+
+
+
+
